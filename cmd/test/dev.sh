234392f03e1d8d1b7552a0fa7198f8cffa3ca9d4
berquerant <sin@sin-pro.local>
2024-04-16 00:23:14 +0900

    Add script for developing

---
+++ cmd/test/dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+d=$(cd $(dirname $0); pwd)
+
+cd "${d}/../.."
+make
+
+grdep="${d}/../../dist/grdep"
+target="test/target"
+cfg="$(mktemp).yml"
+
+cd "${d}/.."
+"$grdep" skeleton > "$cfg"
+echo "$target" | "$grdep" run "$cfg" "$@"
